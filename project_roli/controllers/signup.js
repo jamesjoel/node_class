55593bd76b56bc2c73fa2e3c6626d7692365277c
jamesjoel <31881456+jamesjoel@users.noreply.github.com>
2017-11-27 18:43:39 +0530

    update signup.js

--- project_roli/controllers/signup.js
+++ project_roli/controllers/signup.js
@@ -3,20 +3,40 @@ var router=express.Router();
 var user=require('../models/user');
 
 router.get('/signup', function(req, res){
-	var pageData={ title : "Signup Page", pagename : './signup/index' };
+	var pageData={ title : "Signup Page", pagename : './signup/index', message : req.flash('msg')};
 	res.render('layout', pageData);
 });
 router.post('/signup', function(req, res){
 	delete req.body.cnf_password;
-	user.insert(req.body, function(err, result){
+	user.getWhereOne({ email : req.body.email }, function(err, doc){
 		if(err){
-			console.log('Insert Error', err);
+			console.log('Select error', err);
 			return;
 		}
-		req.flash('msg', 'Signup Complete');
-		res.redirect('/login');
+		if(doc && doc != null)
+		{
+			req.flash("msg", "Email Already Exists");
+			res.redirect('/signup');
+		}
+		else
+		{
+			user.insert(req.body, function(err, result){
+					if(err){
+						console.log('Insert Error', err);
+						return;
+					}
+					req.flash('msg', 'Signup Complete');
+					res.redirect('/login');
+				});
+		}
 	});
+	
 });
 
 
 module.exports=router;
+
+/*
+
+
+*/
